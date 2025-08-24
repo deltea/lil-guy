f0577052da4543326840e1632ddd3bfe28d5bc5c
deltea <thcheetah777@gmail.com>
2025-08-24 00:23:56 -0700

    add some base styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+  <title>lil guy</title>
+</svelte:head>
+
+<h1 class="text-xl font-semibold">hello sup</h1>
