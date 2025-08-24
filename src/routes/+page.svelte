8de614d2a2b1b44460f3063dc0152d6a5d192b13
deltea <thcheetah777@gmail.com>
2025-08-24 00:55:20 -0700

    cool typewriter different method

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
-  import { animate, text, stagger } from "animejs";
+  import { animate } from "animejs";
   import { onMount } from "svelte";
 
-  // let t = $state("");
+  let lines = $state([
+    "hello world!",
+    "how's it going!",
+    "hello there!",
+    "i'm lil guy!"
+  ]);
+  let lineIndex = $state(0);
+  let charIndex = $state(0);
+  let currentLine = $derived(lines[lineIndex].substring(0, charIndex));
 
   onMount(() => {
-    const { chars } = text.split("#typewriter", {
-      chars: { wrap: "clip" }
-    });
-
-    animate(chars, {
-      opacity: [
-        { to: ["0%", "100%"] },
-      ],
-      duration: 150,
-      ease: "out(3)",
-      delay: stagger(40),
-      onUpdate: () => {
-        console.log("update");
-      }
-    });
+    setInterval(() => {
+      // lineIndex = (lineIndex + 1) % lines.length;
+      charIndex = Math.min(lines[lineIndex].length, charIndex + 1);
+    }, 50);
   });
 </script>
 
@@ -34,7 +31,7 @@
     class="w-12"
   >
 
-  <p id="typewriter" class="border-2 border-fg rounded-xl p-4 text-xl font-semibold">
-    hello! how are you today?
+  <p id="typewriter" class="border-2 border-fg rounded-xl py-4 px-6 text-xl font-semibold">
+    {currentLine}
   </p>
 </main>
